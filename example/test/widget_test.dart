10a53bcb0ed7a0d2f69d260afc48cb6abbb2ef0f
Mohammed <mohammed.95mahmoud@gmail.com>
2019-07-14 11:25:07 +0200

    update version

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,21 +1,6 @@
-import 'package:chip_list_choice_example/ChipListSample1.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('chip_list_choice');
 
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
-
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
-
-  testWidgets('ChipList Choice Test', (WidgetTester tester) async {
-    await tester.pumpWidget(ChipListSample1());
-  });
 }
